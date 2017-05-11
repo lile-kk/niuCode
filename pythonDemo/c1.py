218813af8d5e9b378d1d8b6c6251ac13e696b726
xh <catherine_xuh@163.com>
2017-05-11 16:57:20 +0800

    Initial commit

---
+++ pythonDemo/c1.py
@@ -0,0 +1,10 @@
+import request
+
+
+def quishibaike():
+    content = requests.get('http://www.qiushibaike.com').content
+    soup = BeautifulSoup(content, 'html.parser')
+
+    for div in soup.find_all('div', {'class': 'content'}):
+        print
+        div.text.strip()
